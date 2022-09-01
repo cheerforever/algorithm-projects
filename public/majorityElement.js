9f74ffc96da9356c88d89ee740fc15da4150ca2d
wangdanni <wangdanni@bixin.cn>
2022-09-01 10:50:11 +0800

    majorityElement

--- public/majorityElement.js
+++ public/majorityElement.js
@@ -14,10 +14,45 @@
 // 输入：nums = [2,2,1,1,1,2,2]
 // 输出：2
 
+
+// 方法一：
 /**
  * @param {number[]} nums
  * @return {number}
  */
     var majorityElement = function(nums) {
-    
+    let temp = nums[0]
+    let times = 1
+    for(let i = 1; i< nums.length; i++){
+        if(nums[i] === temp){
+            times++
+        } else {
+            times--
+            if(times === 0){
+                temp = nums[i+1]
+                i++
+            }
+        }
+    }
+    return temp
+};
+
+// 方法二：使用了 Map 来存储遍历时的数字，以数字作为key，以重复次数为value，当我们map.get(key)存在时，便将其值加1，否则插入新的数字，并将其值设为1。
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+  var majorityElement = function(nums) {
+    const map = new Map()
+		for(let num of nums){
+			if(!map.get(num)){
+				map.set(num, 1)
+			} else {
+				map.set(num, map.get(num)+1)
+				if(map.get(num) > nums.length/2){
+					return num
+				}
+			}
+		}
 };
