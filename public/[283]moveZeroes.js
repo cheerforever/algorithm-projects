c12426a548d1d8f5aae3f73ceb9c4679cbdb4782
wangdanni <wangdanni@bixin.cn>
2022-09-02 14:56:44 +0800

    update

---
+++ public/[283]moveZeroes.js
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=283 lang=javascript
+ *
+ * [283] 移动零
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+ var moveZeroes = function(nums) {
+    let length = nums.length
+    for(let i = 0; i < length;i++){
+        if(nums[i] === 0){
+            nums.push(0)
+            nums.splice(i, 1)
+            //当移动到末尾的元素，就不用再一次进行遍历了，所以遍历的长度要减去1位
+            length--
+            //当为0的元素删除后，下一个元素就会前进一位占据该位置，所以要从该位置在进行判断
+            i--
+        } 
+    }
+    return nums
+};
+// @lc code=end
---
+++ public/redme.md
@@ -0,0 +1,11 @@
+1. js 插入到指定位置
+
+意思: 在 array 数组的 index 位置插入元素 val
+
+array.splice(index,0,val);
+
+2. js 删除指定位置的元素
+
+意思：删除 index 位置上的元素 第二个参数的意思是 删除从 index 开始的后几位
+
+array.splice(index,1);
