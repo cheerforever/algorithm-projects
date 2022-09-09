da4c0ae9d1f5feba171975466a43fb69dd009e13
wangdanni <dannicheer_chen@163.com>
2022-09-09 16:09:23 +0800

    update

--- leetcode/linked-list/Q21.js
+++ leetcode/linked-list/Q21.js
@@ -83,3 +83,37 @@
 };
 // @lc code=end
 
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+// 方法二：递归
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+ var mergeTwoLists = function(list1, list2) {
+    if(list1 === null) return list2
+    if(list2 === null) return list1
+    function deep(l1, l2){
+        if(l1 === null) return l2
+        if(l2 === null) return l1
+        if(l1.val < l2.val){
+            l1.next = deep(l1.next, l2)
+            return l1
+        } else {
+            l2.next = deep(l1, l2.next)
+            return l2
+        }
+
+    }
+    return deep(list1, list2)
+};
+// @lc code=end
+
