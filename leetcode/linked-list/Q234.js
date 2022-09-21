c9752ca23d292db10528e4f39adb6920dd440373
wangdanni <dannicheer_chen@163.com>
2022-09-21 19:39:35 +0800

    update

---
+++ leetcode/linked-list/Q234.js
@@ -0,0 +1,78 @@
+/*
+ * @lc app=leetcode.cn id=234 lang=javascript
+ *
+ * [234] 回文链表
+ *
+ * https://leetcode.cn/problems/palindrome-linked-list/description/
+ *
+ * algorithms
+ * Easy (52.40%)
+ * Likes:    1513
+ * Dislikes: 0
+ * Total Accepted:    505.5K
+ * Total Submissions: 963K
+ * Testcase Example:  '[1,2,2,1]'
+ *
+ * 给你一个单链表的头节点 head ，请你判断该链表是否为回文链表。如果是，返回 true ；否则，返回 false 。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：head = [1,2,2,1]
+ * 输出：true
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：head = [1,2]
+ * 输出：false
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 链表中节点数目在范围[1, 10^5] 内
+ * 0 <= Node.val <= 9
+ * 
+ * 
+ * 
+ * 
+ * 进阶：你能否用 O(n) 时间复杂度和 O(1) 空间复杂度解决此题？
+ * 
+ */
+
+// 方法一：将值复制到数组中的双指针法
+// 遍历链表，将值保存到一个数组中
+// 使用双指针法判断是否回文
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+ var isPalindrome = function(head) {
+    let values = []
+    while(head){
+        values.push(head.val)
+        head = head.next
+    }
+    for(let i = 0, j = values.length - 1; i < j; i++, j--){
+        if(values[i] !== values[j]){
+            return false
+        }
+    }
+    return true
+};
+// @lc code=end
+
